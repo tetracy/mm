0bc6a1804280f4d583e827c1649cf389a0d0a2fd
Albert <theageofalbert@gmail.com>
2025-04-21 12:40:07 +0800

    Fix lab display

--- mm.sh
+++ mm.sh
@@ -3,7 +3,8 @@ get() {
 	link=$(curl -s https://mehlmanmedical.com/?s="$1" | grep --only-match "https://mehlmanmedical.com/hy-usmle-q-$1-[^/]*/" | head -n 1)
 	echo "opening the $link"
 	curl -s "$link" -o temp.html
-	grep -A 20 "<div class=\"entry-content\">" temp.html  | grep "<p>" | sed 's/<p>//g; s/<\/p>//g'
+	grep -A 20 "<div class=\"entry-content\">" temp.html  | grep "<p>" | sed 's/<p>//g; s/<\/p>//g; /<br/d'
+	grep -A 20 "<div class=\"entry-content\">" temp.html  | grep "br" | sed 's/<p>//g; s/<br \/>//g'
 	grep -A 20 "<div class=\"entry-content\">" temp.html  | grep "radio" | grep --only-match -E ">[^<]*</span></div>$" | sed 's/<\/span><\/div>//g'
 }
 clear
